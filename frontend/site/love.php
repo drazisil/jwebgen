fc449b820999a1720538a218100e56d24627454a
Joe Becher <jwbecher@drazisil.com>
2019-08-15 20:00:34 -0400

    move the restof the frontend into a docker container

---
+++ frontend/site/love.php
@@ -0,0 +1,28 @@
+<?php 
+
+date_default_timezone_set('America/Chicago');
+
+$home_path = '/var/www/html/';
+
+// put full path to Smarty.class.php
+require($home_path . '../smarty/Smarty.class.php');
+$smarty = new Smarty();
+
+$smarty->setTemplateDir($home_path . 'smarty/templates');
+$smarty->setCompileDir($home_path . 'smarty/templates_c');
+$smarty->setCacheDir($home_path . 'smarty/cache');
+$smarty->setConfigDir($home_path . 'smarty/configs');
+
+
+
+$smarty->assign('bodyID', 'love');
+//$smarty->assign('arrTimes', $timeSlots);
+$smarty->display('love.tpl');
+
+
+
+
+
+
+
+
--- frontend/site/starter.php
+++ frontend/site/starter.php
@@ -1,42 +1,22 @@
 <?php 
 
-
-
 date_default_timezone_set('America/Chicago');
-
-
 
 $home_path = '/var/www/html/';
 
-
-
 // put full path to Smarty.class.php
-
 require($home_path . '../smarty/Smarty.class.php');
-
 $smarty = new Smarty();
 
-
-
 $smarty->setTemplateDir($home_path . 'smarty/templates');
-
 $smarty->setCompileDir($home_path . 'smarty/templates_c');
-
 $smarty->setCacheDir($home_path . 'smarty/cache');
-
 $smarty->setConfigDir($home_path . 'smarty/configs');
 
 
 
-
-
-
-
-$smarty->assign('bodyID', 'index');
-
+$smarty->assign('bodyID', 'starter');
 //$smarty->assign('arrTimes', $timeSlots);
-
-$smarty->display('index.tpl');
-
+$smarty->display('starter.tpl');
 
 
