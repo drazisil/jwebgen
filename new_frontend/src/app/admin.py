a04afbb9cc55488853aa9347cb26a68c5ce27561
Joe Becher <jwbecher@drazisil.com>
2019-08-17 10:59:21 -0400

    start of new frontend

--- new_frontend/src/app/admin.py
+++ new_frontend/src/app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Breed, Gene
+
+admin.site.register(Breed)
+admin.site.register(Gene)
--- new_frontend/src/app/models.py
+++ new_frontend/src/app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Breed(models.Model):
+    breed_id = models.IntegerField()
+    breed_name = models.CharField(max_length=12)
+    def __str__(self):
+        return self.breed_name
+
+
+class Gene(models.Model):
+    gene_id = models.IntegerField()
+    gene_name = models.CharField(max_length=20)
+    def __str__(self):
+        return self.gene_name
