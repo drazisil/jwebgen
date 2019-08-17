25177f20f46f02b6a4489df0fcface651c52a556
Joe Becher <jwbecher@drazisil.com>
2019-08-17 08:12:33 -0400

    now that it's working, lets get a better test url

--- e2e_tests/cypress/integration/backend_spec.js
+++ e2e_tests/cypress/integration/backend_spec.js
@@ -1,6 +1,6 @@
 describe("Test controller nextAction", function() {
     it("ponyBreed", function() {
-      cy.visit("localhost:8000/controller.php?nextAction=ponyBreed", { timeout: 50000 });
+      cy.visit("localhost:8000/controller.php?&DadID=1&MomID=2&age=Baby&bspattern=BC&bssocks=OO&bshair=TT&bsface=KK&nextAction=ponyBreed", { timeout: 50000 });
       cy.contains("Father | Mother");
     });
   });
